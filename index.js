c2bbf86035d30ca618f97e4d4bf3fecaf6924f5c
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-09-19 15:56:13 -0400

    Added a new change with asyc method

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const mongoose = require('mongoose');
 const express = require('express');
 const app = express()
 
+const Ositos = mongoose.model('Ositos', new mongoose.Schema({
+  type: String,
+  color: String,
+}))
+
 mongoose.connect('mongodb://nath:12345678@monguito:27017/miapp?authSource=admin')
 
-app.get('/ositos', (_req, res) => {
-  console.log('creando...')
+app.get('/ositos', async (_req, res) => {
+  Ositos.create({ type: 'Grisley', color: 'blue' });
+  console.log('creando...');
+  const osos = await Ositos.find();
   return res.send('ok')
 })
+
+
 
 app.listen(3000, () => console.log('listening...'));
