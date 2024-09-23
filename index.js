4cd907bfd52bf748028109af24a1b2888c657979
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-09-23 13:37:04 -0400

    Ya funciona

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ const Ositos = mongoose.model('Ositos', new mongoose.Schema({
   color: String,
 }))
 
-mongoose.connect('mongodb://nath:12345678@monguito:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://nath:12345678@localhost:27017/miapp?authSource=admin')
 
 app.get('/ositos', async (_req, res) => {
-  Ositos.create({ type: 'Grisley', color: 'blue' });
-  console.log('creando...');
+  console.log('listando...')
   const osos = await Ositos.find();
+  return res.send(osos)
+})
+app.get( async (_req, res) => {
+  console.log('creando...')
+  await Ositos.create({ tipo: 'Grisley', estado: 'Green' })
   return res.send('ok')
 })
-
-
 
 app.listen(3000, () => console.log('listening...'));
