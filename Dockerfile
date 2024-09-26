c4650f602e6e434f19bf0b87d5547de0fcc8d49a
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-09-26 10:13:16 -0400

    Added all chanes about .js

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
-FROM node:18
+FROM node:18-alpine
 
 RUN mkdir -p /home/app
 COPY . /home/app
 EXPOSE 3000
 CMD ["node","/home/app/index.js"]
-
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Ositos = mongoose.model('Ositos', new mongoose.Schema({
   color: String,
 }))
 
-mongoose.connect('mongodb://nath:12345678@localhost:27017/miapp?authSource=admin')
+mongoose.connect('mongodb://nath:12345678@monguito:27017/miapp?authSource=admin')
 
 app.get('/ositos', async (_req, res) => {
   console.log('listando...')
